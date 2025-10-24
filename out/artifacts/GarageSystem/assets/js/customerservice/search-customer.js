0265b7a908fffe98ddc554331d52b0bb6346aa60
Phan Thao <phanthithao.4444@gmail.com>
2025-10-25 04:56:08 +0700

    update main

--- out/artifacts/GarageSystem/assets/js/customerservice/search-customer.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    const fromDate = document.querySelector('input[name="fromDate"]').value;
-    const toDate = document.querySelector('input[name="toDate"]').value;
-
-    if (fromDate && toDate && new Date(fromDate) > new Date(toDate)) {
-        e.preventDefault();
-        alert("The start date cannot be greater than the end date!");
-    }
-});
